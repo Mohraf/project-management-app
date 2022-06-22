6518a4740c95aa88e5d4cb7178a95e34b9ad0788
mohraf <amosngok@gmail.com>
2022-06-22 14:39:46 +0300

    implement cache with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,28 @@ import {
   InMemoryCache
 } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming){
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming){
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
