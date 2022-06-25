8623cdc84f7c4daec558ed6e0e8a23fae2c88db7
mohraf <amosngok@gmail.com>
2022-06-25 21:06:04 +0300

    fetch and display projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Clients from "./components/Clients";
+import Projects from "./components/Projects";
 import {
   ApolloProvider,
   ApolloClient,
@@ -38,6 +39,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects/>
           <Clients/>
         </div>
       </ApolloProvider>
