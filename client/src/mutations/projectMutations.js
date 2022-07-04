efdf7038537416fcf2173531ba0f91447c8362e5
mohraf <amosngok@gmail.com>
2022-07-04 14:36:09 +0300

    update project mutation

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 const ADD_PROJECT = gql`
-    mutation AddProject($name: String! $description: String!, 
-    $status: ProjectStatus!, $clientId: ID!) {
+    mutation AddProject($name: String! $description: String! 
+    $status: ProjectStatus! $clientId: ID!) {
         addProject(name: $name, description: $description,
         status: $status, clientId: $clientId) {
             id
@@ -27,4 +27,23 @@ const DELETE_PROJECT = gql`
     }
 `;
 
-export {ADD_PROJECT, DELETE_PROJECT}
+const UPDATE_PROJECT = gql`
+    mutation UpdateProject($id: ID! $name: String! $description: String! 
+    $status: ProjectStatusUpdate!) {
+        updateProject(id: $id, name: $name, description: $description,
+        status: $status) {
+            id
+            name
+            description
+            status  
+            client {
+                id
+                name
+                email
+                phone
+            }
+        }
+    }
+`;
+
+export {ADD_PROJECT, DELETE_PROJECT, UPDATE_PROJECT}
